c42087d01385aa5ebe6815b8f29c62fb5513d1ad
Thomas Aichlseder <thomas.aichlseder@ibmix.at>
2024-09-16 12:27:18 +0200

    Signals inputs

--- Angular/01-starting-project/src/app/user/user.component.ts
+++ Angular/01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, computed, input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -7,12 +7,21 @@ import { Component, Input } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input({required: true}) avatar!: string;
-  @Input({required: true}) name!: string;
+  //@Input({required: true}) avatar!: string;
+  //@Input({required: true}) name!: string;
 
-  get imagePath() {
-    return 'assets/users/' + this.avatar;
+  avatar = input.required<string>();
+  name = input.required<string>();
+
+  imagePath = computed(() => {
+    return 'assets/users/' + this.avatar();
+  })
+
+  //get imagePath() {
+  //  return 'assets/users/' + this.avatar;
+  //}
+
+  onSelectUser() {
+        
   }
-
-  onSelectUser() {}
 }
