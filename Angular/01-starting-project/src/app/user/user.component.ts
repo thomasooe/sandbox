f21343a7c13ec24587ee30171b5bf04ddd39495f
Thomas Aichlseder <thomas.aichlseder@ibmix.at>
2024-09-17 12:24:55 +0200

    outpu() Function

--- Angular/01-starting-project/src/app/user/user.component.ts
+++ Angular/01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output, computed, input } from '@angular/core';
+import { Component, EventEmitter, Input, Output, output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -10,8 +10,8 @@ export class UserComponent {
   @Input({required: true}) id!: string;
   @Input({required: true}) avatar!: string;
   @Input({required: true}) name!: string;
-
-  @Output() select = new EventEmitter();
+  //@Output() select = new EventEmitter();
+  select = output<string>();
 
   //avatar = input.required<string>();
   //name = input.required<string>();
