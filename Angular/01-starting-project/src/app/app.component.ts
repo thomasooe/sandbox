361f74b2a4c03eab86717d655eaf5fcec261fa5c
Thomas Aichlseder <thomas.aichlseder@ibmix.at>
2024-09-18 15:49:29 +0200

    reverted to newer @if and @for

--- Angular/01-starting-project/src/app/app.component.ts
+++ Angular/01-starting-project/src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { NgFor } from '@angular/common';
-import { NgIf } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { TasksComponent } from './tasks/tasks.component';
@@ -9,7 +7,7 @@ import { DUMMY_USERS } from './dummy-users';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
