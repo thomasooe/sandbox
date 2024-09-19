0debcc3ef432c582064b1fc652465309f112d64e
Thomas Aichlseder <thomas.aichlseder@ibmix.at>
2024-09-19 11:25:30 +0200

    task component with task object coming from tasks

--- Angular/01-starting-project/src/app/tasks/tasks.component.ts
+++ Angular/01-starting-project/src/app/tasks/tasks.component.ts
@@ -1,12 +1,46 @@
 import { Component, Input } from '@angular/core';
+import { TaskComponent } from "./task/task.component";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-  @Input() name?: string;
+  @Input({required: true}) userId!: string;
+  @Input({required: true}) name!: string;
+  
+
+  tasks = [ 
+    {
+    id: 't1',
+    userId: 'u1',
+    title: 'Master Angular',
+    summary: 'Learn all',
+    duedate: '2024-12-31'
+  },
+  {
+    id: 't2',
+    userId: 'u3',
+    title: 'Build first prototype',
+    summary: 'Learn all',
+    duedate: '2024-12-31'
+  },
+  {
+    id: 't3',
+    userId: 'u3',
+    title: 'Prepare issue template',
+    summary: 'Learn all',
+    duedate: '2024-12-31'
+  }
+
+]
+
+ get selectedUserTasks() {
+  console.log('selected user ' + this.userId);
+  
+  return this.tasks.filter((task) => task.userId === this.userId);
+ }
 }
