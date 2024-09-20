3368139014a745c1943ff506d8b996cb53a83118
Thomas Aichlseder <thomas.aichlseder@ibmix.at>
2024-09-20 10:32:34 +0200

    models in external file

--- Angular/01-starting-project/src/app/user/user.component.ts
+++ Angular/01-starting-project/src/app/user/user.component.ts
@@ -1,16 +1,11 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { User } from './user.model';
 
 //type User = {
 //  id: string;
 //  avatar: string;
 //  name: string;
 //};
-
-interface User {
-  id: string;
-  avatar: string;
-  name: string;
-};
 
 @Component({
   selector: 'app-user',
