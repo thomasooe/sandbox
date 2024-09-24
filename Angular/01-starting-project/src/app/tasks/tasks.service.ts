64f61301db04acc4d1cc574dab37a63e1a17207c
Thomas Aichlseder <thomas.aichlseder@ibmix.at>
2024-09-24 14:10:00 +0200

    store tasks in local storage

--- Angular/01-starting-project/src/app/tasks/tasks.service.ts
+++ Angular/01-starting-project/src/app/tasks/tasks.service.ts
@@ -1,4 +1,5 @@
 import { NewTaskData } from "./task/task.model";
+import { Injectable } from "@angular/core";
 
 @Injectable({providedIn: 'root'})
 export class TasksService {
@@ -26,6 +27,14 @@ export class TasksService {
       }
     ];
 
+constructor() {
+    const tasks = localStorage.getItem('tasks')
+
+    if (tasks) {
+        this.tasks = JSON.parse(tasks)
+    }
+}
+
 gerUserTasks(userId: string) {
     return this.tasks.filter((task) => task.userId === userId);
 }
@@ -38,10 +47,16 @@ addTask(taskData: NewTaskData, userId: string) {
         duedate: taskData.date,
         userId: userId
       })
+      this.saveTasks();
 }
 
 removeTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+    this.saveTasks();
+}
+
+private saveTasks()  {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks))
 }
 }
-import { Injectable } from "@angular/core";
+
