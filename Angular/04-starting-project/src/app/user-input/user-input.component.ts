032eea7293483ea36f637c5d27cb86bf59c9aedc
Thomas Aichlseder <thomas.aichlseder@ibmix.at>
2024-09-26 14:59:05 +0200

    results as a Signal

--- Angular/04-starting-project/src/app/user-input/user-input.component.ts
+++ Angular/04-starting-project/src/app/user-input/user-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output, signal } from '@angular/core';
+import { Component, output, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { type InvestmentInput } from '../investment-input.model';
 
@@ -11,7 +11,7 @@ import { type InvestmentInput } from '../investment-input.model';
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
-  @Output() calculate = new EventEmitter<InvestmentInput>();
+  calculate = output<InvestmentInput>();
   enteredInitialInvestment = signal('0');
   enteredAnnualInvestment = signal('0');
   enteredExpectedReturn = signal('5');
