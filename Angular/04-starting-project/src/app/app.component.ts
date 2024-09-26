816b224be5aabfdcc972fda8628343d5ec91b7e5
Thomas Aichlseder <thomas.aichlseder@ibmix.at>
2024-09-26 13:15:41 +0200

    calcucation methid accepting and object parameter

--- Angular/04-starting-project/src/app/app.component.ts
+++ Angular/04-starting-project/src/app/app.component.ts
@@ -9,4 +9,28 @@ import { InvestmentResultsComponent } from './investment-results/investment-resu
   templateUrl: './app.component.html',
   imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent]
 })
-export class AppComponent {}
+export class AppComponent {
+  calculateInvestmentResults(data: {initialInvestment: number, duration: number, expectedReturn: number, annualInvestment: number}) {
+    const {initialInvestment, duration, expectedReturn, annualInvestment} = data;
+    const annualData = [];
+    let investmentValue = initialInvestment;
+  
+    for (let i = 0; i < duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+      investmentValue += interestEarnedInYear + annualInvestment;
+      const totalInterest =
+        investmentValue - annualInvestment * year - initialInvestment;
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested: initialInvestment + annualInvestment * year,
+      });
+    }
+
+    return annualData;
+  }
+}
