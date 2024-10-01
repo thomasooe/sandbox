27298107095433c110d6f37bd115ac8156da812f
Thomas Aichlseder <thomas.aichlseder@ibmix.at>
2024-10-01 16:51:37 +0200

    add event listener to host

--- Angular/06-cmp-deep-dive/src/app/shared/control/control.component.ts
+++ Angular/06-cmp-deep-dive/src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, HostListener, Input, ViewEncapsulation, inject } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -7,8 +7,18 @@ import { Component, Input, ViewEncapsulation } from '@angular/core';
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None,
-  host: {class: 'control'}
+  host: {
+    class: 'control',
+    '(click)': 'onClick()'
+}
 })
 export class ControlComponent {
   @Input({required: true}) label!: string;
+  private el = inject(ElementRef); // programmatic access to the host element
+
+  //@HostListener('click')
+  onClick() {
+    console.log('click');
+    //console.log(this.el);
+  }
 }
