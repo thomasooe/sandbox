a4768c48ec858f6e5c582a1447a4392c455d081e
Thomas Aichlseder <thomas.aichlseder@ibmix.at>
2024-10-01 12:47:31 +0200

    Content Projection Selector

--- Angular/06-cmp-deep-dive/src/app/app.component.ts
+++ Angular/06-cmp-deep-dive/src/app/app.component.ts
@@ -4,12 +4,11 @@ import { ServerStatusComponent } from './dashboard/server-status/server-status.c
 import { TrafficComponent } from './dashboard/traffic/traffic.component';
 import { TicketsComponent } from './dashboard/tickets/tickets.component';
 import { DashboardItemComponent } from './dashboard/dashboard-item/dashboard-item.component';
-import { ButtonComponent } from './shared/button/button.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, ServerStatusComponent, TrafficComponent, TicketsComponent, DashboardItemComponent, ButtonComponent],
+  imports: [HeaderComponent, ServerStatusComponent, TrafficComponent, TicketsComponent, DashboardItemComponent],
   templateUrl: './app.component.html'
 })
 export class AppComponent {
