ccaeb701405c1fcd1f71803b1a145feaaa2564bc
Thomas Aichlseder <thomas.aichlseder@ibmix.at>
2024-10-08 17:17:00 +0200

    ngOnDestroy - remove interval

--- Angular/06-cmp-deep-dive/src/app/dashboard/server-status/server-status.component.ts
+++ Angular/06-cmp-deep-dive/src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,11 +7,13 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent implements OnInit {
+export class ServerStatusComponent implements OnInit, OnDestroy {
   currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+  //private interval: NodeJS.Timeout | undefined;
+  private interval?: ReturnType<typeof setInterval>
 
   ngOnInit() {
-    setInterval(() => {
+    this.interval = setInterval(() => {
       const rnd = Math.random(); // value between 0 - 1
 
       if (rnd < 0.5) {
@@ -22,5 +24,9 @@ export class ServerStatusComponent implements OnInit {
         this.currentStatus = 'unknown';
       }
     }, 5000)
+  }
+
+  ngOnDestroy(): void {
+      clearTimeout(this.interval);
   }
 }
