7ca4ed10f573d99427bb321324f209297208265e
Thomas Aichlseder <thomas.aichlseder@ibmix.at>
2024-10-09 16:41:17 +0200

    access templateValue via @ViewChild

--- Angular/06-cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ Angular/06-cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
 import { FormsModule } from '@angular/forms';
@@ -11,8 +11,11 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
+  @ViewChild('form') form?: ElementRef<HTMLFormElement>;
 
-  onSubmit(titleElement: HTMLInputElement, requestElement: HTMLTextAreaElement) {
+  onSubmit(titleElement: string, requestElement: string) {
     console.dir(titleElement);
-  }
+
+    this.form?.nativeElement.reset();
+    }
 }
