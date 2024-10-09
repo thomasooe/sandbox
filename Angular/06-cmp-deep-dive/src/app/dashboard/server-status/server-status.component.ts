f7b204e0ba10284dbac9e406022589a7ba2eeca0
Thomas Aichlseder <thomas.aichlseder@ibmix.at>
2024-10-09 11:54:42 +0200

    Other Option to implement OnDestroy

--- Angular/06-cmp-deep-dive/src/app/dashboard/server-status/server-status.component.ts
+++ Angular/06-cmp-deep-dive/src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, DestroyRef, OnDestroy, OnInit, inject } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,13 +7,14 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent implements OnInit, OnDestroy {
+export class ServerStatusComponent implements OnInit {
   currentStatus: 'online' | 'offline' | 'unknown' = 'online';
   //private interval: NodeJS.Timeout | undefined;
   private interval?: ReturnType<typeof setInterval>
+  private destroyRef = inject(DestroyRef);
 
   ngOnInit() {
-    this.interval = setInterval(() => {
+    const interval = setInterval(() => {
       const rnd = Math.random(); // value between 0 - 1
 
       if (rnd < 0.5) {
@@ -24,9 +25,13 @@ export class ServerStatusComponent implements OnInit, OnDestroy {
         this.currentStatus = 'unknown';
       }
     }, 5000)
+
+    this.destroyRef.onDestroy(() => {
+      clearInterval(interval)
+    })
   }
 
-  ngOnDestroy(): void {
-      clearTimeout(this.interval);
-  }
+  //ngOnDestroy(): void {
+  //    clearTimeout(this.interval);
+  //}
 }
