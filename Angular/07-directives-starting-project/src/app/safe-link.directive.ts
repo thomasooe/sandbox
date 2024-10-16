996cdd5432803e7da8b129d669fef40a4a875d35
Thomas Aichlseder <thomas.aichlseder@ibmix.at>
2024-10-16 16:25:09 +0200

    directive input parameter

--- Angular/07-directives-starting-project/src/app/safe-link.directive.ts
+++ Angular/07-directives-starting-project/src/app/safe-link.directive.ts
@@ -1,4 +1,4 @@
-import { Directive } from "@angular/core";
+import { Directive, input } from "@angular/core";
 
 @Directive({
     selector: 'a[appSafeLink]',
@@ -8,15 +8,18 @@ import { Directive } from "@angular/core";
     }
 })
 export class SafeLinkDirective{
+    queryParam = input('myapp', {alias: 'appSafeLink'});
+
     constructor() {
         console.log("SafeLinkDirective is active");
-        
     }
 
     onConfirmLeavePage(event: MouseEvent) {
         const wantsToLEave = window.confirm('Do you want to leave the app');
 
         if (wantsToLEave) {
+            const address = (event.target as HTMLAnchorElement).href;
+            (event.target as HTMLAnchorElement).href = address + "?from=" + this.queryParam();
             return;
         }
 
