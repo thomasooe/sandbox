043f5a8e3491219e9f9192db10894d3ce4a5f2b0
Thomas Aichlseder <thomas.aichlseder@ibmix.at>
2024-10-16 16:13:03 +0200

    directive logic

--- Angular/07-directives-starting-project/src/app/safe-link.directive.ts
+++ Angular/07-directives-starting-project/src/app/safe-link.directive.ts
@@ -2,11 +2,24 @@ import { Directive } from "@angular/core";
 
 @Directive({
     selector: 'a[appSafeLink]',
-    standalone: true
+    standalone: true,
+    host: {
+        '(click)': 'onConfirmLeavePage($event)'
+    }
 })
 export class SafeLinkDirective{
     constructor() {
         console.log("SafeLinkDirective is active");
         
+    }
+
+    onConfirmLeavePage(event: MouseEvent) {
+        const wantsToLEave = window.confirm('Do you want to leave the app');
+
+        if (wantsToLEave) {
+            return;
+        }
+
+        event.preventDefault();
     }
 }
