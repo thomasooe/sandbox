fd90eec9fd1a379ca6d63ef30ed68c1bef826eac
Thomas Aichlseder <thomas.aichlseder@ibmix.at>
2024-11-12 14:44:30 +0100

    custom temperaturePipe created

---
+++ Angular/08-pipes-deep-dive/src/app/temperature.pipe.ts
@@ -0,0 +1,42 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({ 
+    name: 'temperature',
+    standalone: true
+})
+export class TemperaturePipe implements PipeTransform {
+    transform(value: string | number | null, inputType: 'cel' | 'fah' , outputType?: 'cel' | 'fah') {
+        let val: number;
+
+        if (!value) {
+            return value;
+        }
+
+        if (typeof value === 'string') {
+            val = parseFloat(value);
+        } else {
+            val = value;
+        }
+
+        let outputTemp: number;
+        if (inputType === 'cel' && outputType === 'fah')
+        {
+            outputTemp = val * (9 / 5) + 32;
+        } else if (inputType === 'fah' && outputType === 'cel')
+        {
+            outputTemp = (val - 32) * (5 / 9);
+        } else {
+            outputTemp = val;
+        }
+        
+        let symbol: '°C' | '°F';
+
+        if (!outputType) {
+            symbol = inputType === 'cel' ? '°C' : '°F';
+        } else {
+            symbol = outputType === 'cel' ? '°C' : '°F';
+        }
+
+        return `${outputTemp.toFixed(2)} ${symbol}`;
+    }
+}
