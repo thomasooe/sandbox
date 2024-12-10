8701eb6568d1baad8f053580f1a17019eb4f6a96
Thomas Aichlseder <thomas.aichlseder@ibmix.at>
2024-12-10 16:28:45 +0100

    injectable alternative

--- Angular/09-services-deep-dive/src/main.ts
+++ Angular/09-services-deep-dive/src/main.ts
@@ -2,4 +2,5 @@ import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
 
+//bootstrapApplication(AppComponent, {providers: [TasksService]}).catch((err) => console.error(err));
 bootstrapApplication(AppComponent).catch((err) => console.error(err));
