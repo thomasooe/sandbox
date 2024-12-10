25fdc5140a519ed40a81e830a38ccac3c9faf004
Thomas Aichlseder <thomas.aichlseder@ibmix.at>
2024-12-10 15:58:46 +0100

    Service Injection

--- Angular/09-services-deep-dive/src/app/tasks/new-task/new-task.component.ts
+++ Angular/09-services-deep-dive/src/app/tasks/new-task/new-task.component.ts
@@ -1,5 +1,6 @@
-import { Component, ElementRef, viewChild } from '@angular/core';
+import { Component, ElementRef, inject, viewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-new-task',
@@ -10,8 +11,11 @@ import { FormsModule } from '@angular/forms';
 })
 export class NewTaskComponent {
   private formEl = viewChild<ElementRef<HTMLFormElement>>('form');
+  
+  constructor (private tasksService: TasksService) {};
 
   onAddTask(title: string, description: string) {
     this.formEl()?.nativeElement.reset();
+    this.tasksService.addTask({ title, description });
   }
 }
