228c4ac67ce2b8e778cc99c55a695fb261af033f
Thomas Aichlseder <thomas.aichlseder@ibmix.at>
2024-12-10 16:22:15 +0100

    Status Switch and Filter

--- Angular/09-services-deep-dive/src/app/tasks/tasks.service.ts
+++ Angular/09-services-deep-dive/src/app/tasks/tasks.service.ts
@@ -1,12 +1,12 @@
 import { Injectable, signal } from "@angular/core";
-import { Task } from "./task.model";
+import { Task, TaskStatus } from "./task.model";
 
 @Injectable({ providedIn: 'root' })
 export class TasksService {
     private tasks = signal<Task[]>([]);
 
     allTasks = this.tasks.asReadonly();
-    
+
     addTask(taskData: { title: string, description: string }) {
         const newTask: Task = {
             ...taskData,
@@ -15,5 +15,9 @@ export class TasksService {
         };
 
         this.tasks.update((oldTasks) => [...oldTasks, newTask]);
+    }
+
+    updateTaskStatus(taskId: string, newStatus: TaskStatus) {
+        this.tasks.update((oldTasks) => oldTasks.map((task) => task.id === taskId ? { ...task, status: newStatus } : task));
     }
 }
