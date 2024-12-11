dd26d7868afa1b2f51dd212e9769fc81bd75ca37
Thomas Aichlseder <thomas.aichlseder@ibmix.at>
2024-12-11 11:11:03 +0100

    inject Service in Service

--- Angular/09-services-deep-dive/src/app/tasks/tasks.service.ts
+++ Angular/09-services-deep-dive/src/app/tasks/tasks.service.ts
@@ -1,9 +1,11 @@
-import { Injectable, signal } from "@angular/core";
+import { Injectable, inject, signal } from "@angular/core";
 import { Task, TaskStatus } from "./task.model";
+import { LoggingService } from "../logging.service";
 
 @Injectable({ providedIn: 'root' })
 export class TasksService {
     private tasks = signal<Task[]>([]);
+    private logingService = inject(LoggingService);
 
     allTasks = this.tasks.asReadonly();
 
@@ -15,9 +17,11 @@ export class TasksService {
         };
 
         this.tasks.update((oldTasks) => [...oldTasks, newTask]);
+        this.logingService.log('ADDED TASK');
     }
 
     updateTaskStatus(taskId: string, newStatus: TaskStatus) {
         this.tasks.update((oldTasks) => oldTasks.map((task) => task.id === taskId ? { ...task, status: newStatus } : task));
+        this.logingService.log('STATUS CHANGED');
     }
 }
