5fefb6c2fe1546fa44f8e83b207068d5ca8e2ccf
Thomas Aichlseder <thomas.aichlseder@ibmix.at>
2024-12-13 10:14:26 +0100

    setTimeout example

--- Angular/10-change-detection/src/app/counter/counter.component.ts
+++ Angular/10-change-detection/src/app/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 
 import { InfoMessageComponent } from '../info-message/info-message.component';
 
@@ -9,12 +9,18 @@ import { InfoMessageComponent } from '../info-message/info-message.component';
   styleUrl: './counter.component.css',
   imports: [InfoMessageComponent],
 })
-export class CounterComponent {
+export class CounterComponent implements OnInit {
   count = signal(0);
 
   get debugOutput() {
     console.log('[Counter] "debugOutput" binding re-evaluated.');
     return 'Counter Component Debug Output';
+  }
+
+  ngOnInit() {
+      setTimeout(() => {
+        this.count.set(0);
+      }, 4000);
   }
 
   onDecrement() {
