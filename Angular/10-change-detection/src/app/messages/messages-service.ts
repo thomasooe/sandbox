6304f44cf941e447c2de580a61849a151642a339
Thomas Aichlseder <thomas.aichlseder@ibmix.at>
2025-02-05 14:12:01 +0100

    reverted to Signals

--- Angular/10-change-detection/src/app/messages/messages-service.ts
+++ Angular/10-change-detection/src/app/messages/messages-service.ts
@@ -1,21 +1,14 @@
-import { Injectable } from "@angular/core";
-import { BehaviorSubject } from "rxjs";
+import { Injectable, signal } from "@angular/core";
 
 @Injectable({
     providedIn: 'root',
 })
 
 export class MessagesService {
-    messages$ = new BehaviorSubject<string[]>([]);
-    private messages: string[] = [];
-
-    get allMessages() {
-        return [...this.messages];
-    }
-    
+    private messages = signal<string[]>([]);
+    allMessages = this.messages.asReadonly();
 
     addMessage(message: string) {
-        this.messages = [...this.messages, message];
-        this.messages$.next(this.messages);
+        this.messages.update(oldMessages => [...oldMessages, message]);
     }
 }
