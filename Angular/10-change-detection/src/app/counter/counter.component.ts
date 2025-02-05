b807ffa3d0f0d7153adbf1d005734ca8118ae102
Thomas Aichlseder <thomas.aichlseder@ibmix.at>
2025-02-05 14:36:15 +0100

    rxjs start project

--- Angular/10-change-detection/src/app/counter/counter.component.ts
+++ Angular/10-change-detection/src/app/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, NgZone, OnInit, inject, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, signal } from '@angular/core';
 
 import { InfoMessageComponent } from '../info-message/info-message.component';
 
@@ -11,7 +11,6 @@ import { InfoMessageComponent } from '../info-message/info-message.component';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CounterComponent implements OnInit {
-  private zone = inject(NgZone);
   count = signal(0);
 
   get debugOutput() {
@@ -20,15 +19,13 @@ export class CounterComponent implements OnInit {
   }
 
   ngOnInit() {
-      setTimeout(() => {
-        this.count.set(0);
-      }, 4000);
+    setTimeout(() => {
+      this.count.set(0);
+    }, 4000);
 
-      this.zone.runOutsideAngular(() => {
-        setTimeout(() => {
-          console.log('timer expired');
-        }, 5000);
-      });
+    setTimeout(() => {
+      console.log('timer expired');
+    }, 5000);
   }
 
   onDecrement() {
