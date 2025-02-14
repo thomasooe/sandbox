b4db229363f76876bdc3a4054c79b892255c7ba4
Thomas Aichlseder <thomas.aichlseder@ibmix.at>
2025-02-14 15:36:39 +0100

    custom Observer Object

--- Angular/11-rxjs/src/app/app.component.ts
+++ Angular/11-rxjs/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, DestroyRef, OnInit, effect, inject, signal } from '@angular/core';
 import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 
-import { interval, map } from 'rxjs';
+import { Observable, interval, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -15,23 +15,47 @@ export class AppComponent implements OnInit {
   interval$ = interval(1000);
   intervalSignal = toSignal(this.interval$, { initialValue: 999 });
 
+  customInterval$ = new Observable((subscriber) => {
+    let timesExecuted = 0;
+    const interval = setInterval(() => {
+      //if (timesExecuted > 2) {
+      //  subscriber.error('abcError')
+      //}
+      if (timesExecuted > 3) {
+        clearInterval(interval);
+        subscriber.complete();
+        return;
+      }
+      console.log('Emitting new value...')
+      subscriber.next({ message: 'New value' });
+      timesExecuted++;
+    }, 2000);
+  });
+
   constructor() {
     //effect(() => {
     //  console.log(`Clicked Button ${this.clickCount()} times.`)
     //});
 
-    const subscription = this.clickCount$.subscribe({ 
+    const subscription1 = this.customInterval$.subscribe({
+      next: (val) => console.log(val),
+      complete: () => console.log('Interval completed'),
+      error: (err) => console.log(err)
+    });
+
+    const subscription = this.clickCount$.subscribe({
       next: (val) => console.log(`$Clicked Button ${this.clickCount()} times.`)
     });
     this.destroyRef.onDestroy(() => {
+      subscription1.unsubscribe();
       subscription.unsubscribe();
     })
   }
 
   ngOnInit(): void {
     const subscription = interval(1000).pipe(
-      map((val) => val*2)
-    ).subscribe({ 
+      map((val) => val * 2)
+    ).subscribe({
       next: (val) => console.log(val)
     });
 
