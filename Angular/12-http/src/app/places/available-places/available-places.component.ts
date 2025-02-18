3630b4ec4e750d054ee8d9decee03e49e4c2063c
Thomas Aichlseder <thomas.aichlseder@ibmix.at>
2025-02-18 13:58:43 +0100

    get request to /places

--- Angular/12-http/src/app/places/available-places/available-places.component.ts
+++ Angular/12-http/src/app/places/available-places/available-places.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, DestroyRef, OnInit, inject, signal } from '@angular/core';
 
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
@@ -12,8 +12,21 @@ import { HttpClient } from '@angular/common/http';
   styleUrl: './available-places.component.css',
   imports: [PlacesComponent, PlacesContainerComponent]
 })
-export class AvailablePlacesComponent {
+export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
 
   private httpClient = inject(HttpClient);
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit() {
+        const subscription = this.httpClient.get<{places: Place[]}>('http://localhost:3000/places').subscribe({
+          next: (resData) => {
+            console.log(resData.places);
+          }
+        }); /// Port 3000 ist der default port
+
+        this.destroyRef.onDestroy(() => {
+          subscription.unsubscribe();
+        })
+  }
 }
