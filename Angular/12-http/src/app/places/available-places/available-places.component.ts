3d17fc8de6d6a173a8430df8f321c8ea796ef5f9
Thomas Aichlseder <thomas.aichlseder@ibmix.at>
2025-02-18 13:49:01 +0100

    httpClient setup

--- Angular/12-http/src/app/places/available-places/available-places.component.ts
+++ Angular/12-http/src/app/places/available-places/available-places.component.ts
@@ -1,16 +1,19 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-available-places',
   standalone: true,
   templateUrl: './available-places.component.html',
   styleUrl: './available-places.component.css',
-  imports: [PlacesComponent, PlacesContainerComponent],
+  imports: [PlacesComponent, PlacesContainerComponent]
 })
 export class AvailablePlacesComponent {
   places = signal<Place[] | undefined>(undefined);
+
+  private httpClient = inject(HttpClient);
 }
