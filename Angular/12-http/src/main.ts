31fdf8c2347d9c5d20a256dbd14cc7d4fd66c627
Thomas Aichlseder <thomas.aichlseder@ibmix.at>
2025-02-19 16:32:49 +0100

    responce interceptors

--- Angular/12-http/src/main.ts
+++ Angular/12-http/src/main.ts
@@ -1,7 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { HttpEventType, HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { tap } from 'rxjs';
 
 
 function loggingInterceptor( request: HttpRequest<unknown>, next: HttpHandlerFn) {
@@ -11,7 +12,17 @@ function loggingInterceptor( request: HttpRequest<unknown>, next: HttpHandlerFn)
     //const req = request.clone({
     //    headers: request.headers.set('X-DEBUG', 'TESTING')
     //});
-    return next(request);
+    return next(request).pipe(
+        tap({
+            next: event => {
+                if (event.type === HttpEventType.Response) {
+                    console.log('incoming response');
+                    console.log(event.status);
+                    console.log(event.body);
+                }
+            }
+        })
+    );
 }
 
 bootstrapApplication(AppComponent, {
