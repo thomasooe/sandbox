16651b23c4d657995f92ab6ac901cdbefed16c9b
Thomas Aichlseder <thomas.aichlseder@ibmix.at>
2025-02-19 13:08:08 +0100

    getting and showing user-places

--- Angular/12-http/src/app/places/user-places/user-places.component.ts
+++ Angular/12-http/src/app/places/user-places/user-places.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, OnInit, inject, signal } from '@angular/core';
 
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { PlacesComponent } from '../places.component';
+import { HttpClient } from '@angular/common/http';
+import { Place } from '../place.model';
+import { catchError, map, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-user-places',
@@ -10,5 +13,50 @@ import { PlacesComponent } from '../places.component';
   styleUrl: './user-places.component.css',
   imports: [PlacesContainerComponent, PlacesComponent],
 })
-export class UserPlacesComponent {
+export class UserPlacesComponent implements OnInit {
+  user_places = signal<Place[] | undefined>(undefined);
+
+  isFetching = signal(false);
+  error = signal('');
+
+  private httpClient = inject(HttpClient);
+  private destroyRef = inject(DestroyRef);
+
+
+  ngOnInit() {
+    this.isFetching.set(true);
+      const subscription = this.httpClient.get<{places: Place[]}>('http://localhost:3000/user-places')
+      .pipe(
+        map((resData) => resData.places), catchError((error) => 
+        {
+          console.log(error);
+          return throwError(
+            () => new Error('Server Error fetching favorite places. Try again later')
+            );
+          })
+      )
+      .subscribe({
+        next: (places) => {
+          console.log(places);
+          this.user_places.set(places);
+          //console.log(response.body?.places);
+        },
+        complete: () => {
+          this.isFetching.set(false);
+        },
+        error: (error) => {
+          //console.log(error);
+          this.error.set(error.message);
+          //this.error.set("Shome Server Error happened. Try again later");
+        }
+      }); /// Port 3000 ist der default port
+
+      this.destroyRef.onDestroy(() => {
+        subscription.unsubscribe();
+      })
+}
+
+onUnSelectPlace(selectedPlace: Place) {
+
+}
 }
