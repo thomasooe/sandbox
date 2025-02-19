23e9ee19a06f03a2d4e44746298581d3838725a9
Thomas Aichlseder <thomas.aichlseder@ibmix.at>
2025-02-19 16:27:00 +0100

    http interceptor

--- Angular/12-http/src/main.ts
+++ Angular/12-http/src/main.ts
@@ -1,9 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
 
+
+function loggingInterceptor( request: HttpRequest<unknown>, next: HttpHandlerFn) {
+    console.log('HTTP Action');
+    console.log(request);
+
+    //const req = request.clone({
+    //    headers: request.headers.set('X-DEBUG', 'TESTING')
+    //});
+    return next(request);
+}
 
 bootstrapApplication(AppComponent, {
-        providers: [provideHttpClient()]
+        providers: [provideHttpClient(withInterceptors([loggingInterceptor]))]
     }).catch((err) => console.error(err));
