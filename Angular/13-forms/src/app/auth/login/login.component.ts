ad8f50e90bcd54330f05da64186784ed29f9b1b4
Thomas Aichlseder <thomas.aichlseder@ibmix.at>
2025-02-27 15:51:00 +0100

    validation

--- Angular/13-forms/src/app/auth/login/login.component.ts
+++ Angular/13-forms/src/app/auth/login/login.component.ts
@@ -11,6 +11,11 @@ import { FormsModule, NgForm } from '@angular/forms';
 export class LoginComponent {
 
   onSubmit(formData: NgForm) {
+    
+    if (formData.form.invalid) {
+      return;
+    }
+
     const enteredEmail = formData.form.value.email;    
     const enteredPassword = formData.form.value.password;    
 
