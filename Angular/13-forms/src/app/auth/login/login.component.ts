495276b9ec7ce3b9580878ea347a235f6b5c858b
Thomas Aichlseder <thomas.aichlseder@ibmix.at>
2025-02-28 13:45:05 +0100

    saving form Values on every key entry

--- Angular/13-forms/src/app/auth/login/login.component.ts
+++ Angular/13-forms/src/app/auth/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, afterNextRender, inject, viewChild } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
@@ -9,6 +9,18 @@ import { FormsModule, NgForm } from '@angular/forms';
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  private form = viewChild.required<NgForm>('form');
+  private destroyRef = inject(DestroyRef);
+ 
+  constructor() {
+    afterNextRender(() => {
+      const subscription = this.form().valueChanges?.subscribe({
+        next: (value) => console.log(value)
+      });
+
+      this.destroyRef.onDestroy(() => subscription?.unsubscribe());
+    });
+  }
 
   onSubmit(formData: NgForm) {
     
@@ -19,6 +31,9 @@ export class LoginComponent {
     const enteredEmail = formData.form.value.email;    
     const enteredPassword = formData.form.value.password;    
 
+    console.log(formData.form)
     console.log(enteredEmail, enteredPassword);
+
+    formData.form.reset();
   }
 }
